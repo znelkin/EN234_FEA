a73120aaaf41dd8e82b840f506753e671f906056
znelkin <znelkin@gmail.com>
2017-10-06 17:43:07 -0400

    HW 3

--- EN234_FEA/user_codes/src/abaqus_uel_2D.for
+++ EN234_FEA/user_codes/src/abaqus_uel_2D.for
@@ -170,7 +170,7 @@
           dxidx(2,2) = dxdxi(1,1)
           dxidx(1,2) = -dxdxi(1,2)
           dxidx(2,1) = -dxdxi(2,1)
-          !         call abq_inverse_LU(dxdxi,dxidx,2)
+
           dNdx(1:NNODE,1:2) = matmul(dNdxi(1:NNODE,1:2),dxidx)
           B = 0.d0
           B(1,1:2*NNODE-1:2) = dNdx(1:NNODE,1)
