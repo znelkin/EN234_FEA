ec0d1cd108d7f3c5ec338182d670044fc8b1cff7
znelkin <znelkin@gmail.com>
2017-12-18 07:12:40 -0500

    10

--- EN234_Visual_Studio/EN234_Visual_Studio/abaqus_uel_continuum_beam.for
+++ EN234_Visual_Studio/EN234_Visual_Studio/abaqus_uel_continuum_beam.for
@@ -11,7 +11,7 @@
 !          abq_UEL_1D_integrationpoints(n_points, n_nodes, xi, w)  = defines integration points for 1D line integral
 !=========================== ABAQUS format user element subroutine ===================
 
-      SUBROUTINE UEL(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
+      SUBROUTINE UELc(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
      1     PROPS,NPROPS,COORDS,MCRD,NNODE,U,DU,V,A,JTYPE,TIME,DTIME,
      2     KSTEP,KINC,JELEM,PARAMS,NDLOAD,JDLTYP,ADLMAG,PREDEF,NPREDF,
      3     LFLAGS,MLVARX,DDLMAG,MDLOAD,PNEWDT,JPROPS,NJPROP,PERIOD)
@@ -310,7 +310,7 @@
       return
       
 
-      END SUBROUTINE UEL
+      END SUBROUTINE UELc
     
       subroutine psi(step,value)
       
